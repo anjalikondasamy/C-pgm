8c7419328a72bc65b9bcdeb96a2ba784acec14fc
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-02 16:59:56 +0530

    cpgm
    good

---
+++ positiveinteger.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,i,sum=0;
+
+printf("\n Enter the positive integer");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+sum+=i;
+}
+printf("\n sum=%d");
+return0;
+
+}
