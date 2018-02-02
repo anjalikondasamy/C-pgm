0a01cce9bc191da36fa278dc0fb4660914981e23
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-02 16:36:24 +0530

    c pgm
    good

---
+++ largenum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int num;
+char choice;
+
+printf("\n Enter the three numbers");
+scanf("%d",&threenumbers");
+
+printf("\n Enter thefirstnumber");
+scanf("%d",&firstnumber);
+printf("\n Enter the secondnumber");
+scanf("%d",&secondnumber");
+printf("\n Enter the thirdnumber");
+scanf("%d",&thirdnumber");
+
+if(n=1;1<=n;1++)
+{
+printf("\n Enter the largenumber");
+scanf("%d",&largenumber);
+else
+printf("\n ");
+return 0;
+}
+}
