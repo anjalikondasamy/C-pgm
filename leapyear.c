2450c5648f25c9fc1bca9f0299d2f3886c1717bd
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-02 16:46:44 +0530

    cpgm

---
+++ leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int year;
+
+printf("\n Enter a year");
+scanf("%d",year);
+
+if(%400==0)
+printf("\n Enter the leapyear");
+scanf("%d",leapyear);
+else
+printf("\n Enter the not leapyear");
+scanf("%d",notleapyear);
+return0;
+}
